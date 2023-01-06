849e7ba7b5be52d8453644d5fab87ec8ca8b2813
Jörg Vogt <joerg.vogt@htw-dresden.de>
2023-01-06 18:05:40 +0100

    JavaDoc hinzugefügt

--- src/FT.java
+++ src/FT.java
@@ -1,8 +1,20 @@
 import java.io.IOException;
 
 public interface FT {
-
+  
+  /**
+   * Client: request to send a whole file
+   *
+   * @return true if file exists and can be sent
+   * @throws IOException
+   */
   boolean file_req() throws IOException;
-
+  
+  /**
+   * Server: request to receive a whole file (using an ARQ-protocol)
+   *
+   * @return true if file received correctly
+   * @throws IOException
+   */
   boolean file_init() throws IOException;
 }
